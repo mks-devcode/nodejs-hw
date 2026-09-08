724ee3831462f11d32245d3db0045fdc537abf43
Maks <pochtalion777@gmail.com>
2026-09-08 22:04:28 +0200

    feat: complete hw1

--- src/server.js
+++ src/server.js
@@ -1,3 +1,44 @@
-const message = import.meta.dirname;
-console.log(message);
-console.log("Hello");
+import express from 'express';
+import 'dotenv/config';
+import cors from 'cors';
+
+const app = express();
+const PORT = process.env.PORT ?? 3000;
+
+app.use(express.json());
+app.use(cors());
+
+app.get('/notes', (req, res) => {
+  res.status(200).json({
+	message: "Retrieved all notes"
+});
+});
+
+app.get('/notes/:noteId', (req, res) => {
+  const { noteId } = req.params;
+  console.log("noteId:", noteId);
+  res.status(200).json({
+    message: `Retrieved note with ID: ${noteId}`
+  });
+});
+
+app.get('/test-error', () => {
+  throw new Error('Simulated server error');
+});
+
+app.use((req, res) => {
+  res.status(404).json({
+  "message": "Route not found"
+});
+});
+
+app.use((err, req, res, next) => {
+  console.error('Error:', err.message);
+  res.status(500).json({
+    message: err.message,
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
